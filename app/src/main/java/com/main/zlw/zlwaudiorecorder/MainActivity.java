dd4ce2116bc342b3699203ea0752ffed86119bed
luxiaoyang <luxy25@gmail.com>
2022-02-12 14:39:13 +0800

    提交代码

---
+++ app/src/main/java/com/main/zlw/zlwaudiorecorder/MainActivity.java
@@ -0,0 +1,16 @@
+package com.main.zlw.zlwaudiorecorder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
